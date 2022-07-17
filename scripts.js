4046523061cbc754d9b46b36b194e3a5a22f446f
EthanxLong <exlong6@gmail.com>
2022-07-17 01:38:00 -0400

    Updated js and html

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
 fetch("API_pricing-1657772478846.json")
 .then(function(resp) {return resp.json() })
 .then(function(data) {
+
+    console.log(data.data.pricing)
     
-    machineTypes = data.data.meta_data.machine_types
-    machineServices = data.data.meta_data.services
+    
+    machineTypes = Object.keys(data.data.pricing)
+    machineServices = data.data.pricing
     machinePlans = data.data.meta_data.plans
     machinePricing = data.data.pricing
 
     //console.log(machineServices)
 
     for (i = 0; i < machineTypes.length; i++){
-
         option = document.createElement('option')
         option.text = machineTypes[i]
-
         document.getElementById('machine-dropdown').add(option)
 
     }
@@ -21,14 +22,12 @@ fetch("API_pricing-1657772478846.json")
     for (i = 0; i < machineServices.length; i++){
         option = document.createElement('option')
         option.text = machineServices[i]
-
         document.getElementById('renderer-dropdown').add(option)
     } 
 
     for (i = 0; i < machinePlans.length; i++){
         option = document.createElement('option')
         option.text = machinePlans[i]
-
         document.getElementById('plans-dropdown').add(option)
     }
 
@@ -38,18 +37,34 @@ inputs = [];
 const calculate = (event) =>{
     event.preventDefault();
     let input = {
-        machine: document.getElementById('machine-dropdown').value,
-        renderer: document.getElementById('renderer-dropdown').value,
-        plans: document.getElementById('plans-dropdown').value,
-        totalframes: document.getElementById('totalFrames-input').value,
-        averageframes: document.getElementById('AVGframes-input').value
+        machineInput: document.getElementById('machine-dropdown').value,
+        rendererInput: document.getElementById('renderer-dropdown').value,
+        plansInput: document.getElementById('plans-dropdown').value,
+        totalframesInput: document.getElementById('totalFrames-input').value,
+        averageframesInput: document.getElementById('AVGframes-input').value
     }
     inputs.push(input);
-    document.forms[0].reset();
+
+    let machineInput = inputs[0].machineInput
+    let rendererInput = inputs[0].rendererInput
+    let plansInput = inputs[0].plansInput
+    let totalframesInput = inputs[0].totalframesInput
+    let averageframesInput = inputs[0].averageframesInput
     
-    console.warn('added', {inputs} );
-    let pre = document.querySelector('#msg pre')
-    pre.textContent = '\n' + JSON.stringify(inputs, '\t', 2)
+    // console.log(machineInput)
+    // document.forms[0].reset();
+    
+    
+    fetch("API_pricing-1657772478846.json")
+    .then(function(resp) {return resp.json() })
+    .then(function(data) {
+        machinePrice = data.data.pricing[machineInput][rendererInput][plansInput].cost
+        
+        
+        let price = (averageframesInput/60) * (totalframesInput * machinePrice)
+        document.getElementById('output').innerHTML = price
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
