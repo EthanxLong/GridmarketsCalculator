e57032c3e103b053c57f13d422974fb3327b4bf6
Ethan Long <ethanlong1227@icloud.com>
2022-07-20 17:30:00 -0400

    Fixed Order

--- scripts.js
+++ scripts.js
@@ -5,74 +5,94 @@ fetch("API_pricing-1657772478846.json")
     
     
     machineTypes = Object.keys(data.data.pricing)
+    machineServices = data.data.meta_data.services
+
+    //console.log(data.data.pricing)
     
-    for (i = 0; i < machineTypes.length; i++){
+    for (i = 0; i < machineServices.length; i++){
         option = document.createElement('option')
-        option.text = machineTypes[i]
-        document.getElementById('machine-dropdown').add(option)
+        option.text = machineServices[i]
+        document.getElementById('renderer-dropdown').add(option)
     }
 
     document
-        .getElementById("machine-dropdown")
+          .getElementById("machine-dropdown")
+          .addEventListener("change", (ev) => {
+            speed = ev.target.value
+            machineValue = document.getElementById('machine-dropdown').value
+            renderValue = document.getElementById('renderer-dropdown').value
+            planValue = document.getElementById('plans-dropdown').value
+
+            selectedSpeed = Object.keys(data.data.pricing[machineValue][renderValue])
+  
+            if (!(selectedSpeed.includes(planValue))){
+              $('#plans-dropdown').empty();
+              $('#plans-dropdown').append('<option value="" disabled selected>Select Plan </option>')
+              for (i = 0; i < selectedSpeed.length; i++){
+                  option = document.createElement('option')
+                  option.text = selectedSpeed[i]
+                  document.getElementById("plans-dropdown").add(option)
+                }
+            }
+  
+  
+});
+
+    document
+        .getElementById("renderer-dropdown")
         .addEventListener("change", (e) => {
           machineValue = document.getElementById('machine-dropdown').value
           renderValue = document.getElementById('renderer-dropdown').value
-          machine = e.target.value
-          rendererArray = Object.keys(data.data.pricing[machine])
+          renderer = e.target.value
+          machineArray = data.data.pricing
 
-          
-          if (!(rendererArray.includes(renderValue))){
-            $("#renderer-dropdown").empty();
-            $('#renderer-dropdown').append('<option value="" disabled selected>Select Service </option>')
-            for (i = 0; i < rendererArray.length; i++){
+          //console.log(Object.entries(machineArray))
+          lst = [];
+
+          if (!(lst.includes(machineValue))){
+            $("#machine-dropdown").empty();
+            $('#machine-dropdown').append('<option value="" disabled selected>Select Service </option>')
+            for (const [key, value] of Object.entries(data.data.pricing)) {
+              if (renderValue in value){
+                lst.push(value)
                 option = document.createElement('option')
-                option.text = rendererArray[i]
-                document.getElementById('renderer-dropdown').add(option)
-              } 
-             console.log(rendererArray) 
-          }
+                option.text = key
+                document.getElementById('machine-dropdown').add(option)
+              }
+              }
+            }
 
+          //if (!(machineArray.includes(renderValue))){
+          //  $("#renderer-dropdown").empty();
+          //  $('#renderer-dropdown').append('<option value="" disabled selected>Select Service </option>')
+          //  for (i = 0; i < machineArray.length; i++){
+          //      option = document.createElement('option')
+          //      option.text = machineArray[i]
+          //      document.getElementById('renderer-dropdown').add(option)
+          //    } 
+          //   console.log(machineArray) 
+          //}
+//
 
     
         });
 
-        document
-        .getElementById("renderer-dropdown")
-        .addEventListener("change", (ev) => {
-          speed = ev.target.value
-          machineValue = document.getElementById('machine-dropdown').value
-          renderValue = document.getElementById('renderer-dropdown').value
-          planValue = document.getElementById('plans-dropdown').value
 
-          selectedSpeed = Object.keys(data.data.pricing[machineValue][renderValue])
-          
-          if (!(selectedSpeed.includes(planValue))){
-            $('#plans-dropdown').empty();
-            $('#plans-dropdown').append('<option value="" disabled selected>Select Plan </option>')
-            for (i = 0; i < selectedSpeed.length; i++){
-                option = document.createElement('option')
-                option.text = selectedSpeed[i]
-                document.getElementById("plans-dropdown").add(option)
-              }
-          }
-          
-          
-        });
         
 
-    document.getElementById("btn").addEventListener("click", myFunction);
-
+    document.getElementById("btn").addEventListener("click", myFunction);   
     function myFunction() {
-
+    
         machineValue = document.getElementById("machine-dropdown").value
         renderValue = document.getElementById("renderer-dropdown").value
         plansValue = document.getElementById("plans-dropdown").value
-
+    
         averageframesInput = document.getElementById("AVGframes-input").value
         totalframesInput = document.getElementById("totalFrames-input").value
-
+    
         machineCost = data.data.pricing[machineValue][renderValue][plansValue].cost
-
+    
+    
         
         price = (averageframesInput/60) * (totalframesInput * machineCost)
         
@@ -80,10 +100,9 @@ fetch("API_pricing-1657772478846.json")
             $("#output").empty()
         }
         document.getElementById("output").innerHTML += "Cost: " + price.toFixed(2);
-
-        
-    }
-
+      
+        console.log(averageframesInput, totalframesInput, machineCost)
+    }   
         
 });
 
